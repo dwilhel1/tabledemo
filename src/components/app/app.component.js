462b979f04ef6a2b45ec029d14cc1b5883254d2a
Douglas Wilhelm <dwilhelm@vacayhomeconnect.com>
2019-06-30 19:33:40 -0500

    Add additional table styling and data, add mdi icons package

--- src/components/app/app.component.js
+++ src/components/app/app.component.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import MenuDownIcon from 'mdi-react/MenuDownIcon';
+
 import agent from '../../services/agent';
 import { getInitial } from '../../utilities/string';
 
@@ -13,10 +15,10 @@ class App extends React.Component {
             isLoaded: {
                 contacts: false,
                 deals: false,
-                tags: true,
             },
             contacts: [],
-            columns: ['', 'Contact', 'Total Value', 'Location', 'Deals', 'Tags'],
+            columns: ['', 'ID', 'Contact', 'Phone', 'Deals', 'IP', ''],
+            actions: ['Delete', 'Update'],
         };
     }
 
@@ -31,17 +33,6 @@ class App extends React.Component {
                     contacts: contactsListResult.contacts,
                 });
                 this.state.contacts.forEach((contact, index) => {
-                    // console.log(contact.links);
-                    // console.log(contact.geoAddresses);
-                    agent.Contacts.getContactData(contact.id).then((result) => console.log(result.contactDatum));
-                    // agent.Address.getContactAddress(contact.id).then((result) => console.log(result));
-                    /* agent.Tags.getContactTag(contact.id).then((contactTagResult) => {
-                        console.log(contactTagResult.tag.tag);
-                        /* this.setState({
-                            ...this.state.contacts,
-                        });
-                    }); */
-
                     agent.Contacts.getContactDeals(contact.id).then((contactDealsResult) => {
                         const contacts = this.state.contacts;
                         contacts[index].custom = {
@@ -91,41 +82,54 @@ class App extends React.Component {
                 <table>
                     <thead>
                         <tr className='color-slate-400'>
-                            {columns.map((item, index) => (<th className={index === 0 ? 'padding-left-m' : index === (columns.length - 1) ? 'padding-right-m' : ''} key={item}>{item}</th>))}
+                            {columns.map((item, index) => (<th className={index === 0 ? 'padding-left-m' : index === (columns.length - 1) ? 'padding-right-m' : ''} key={index}>{item}</th>))}
                         </tr>
                     </thead>
-                    <tbody>{contacts.length ? contacts.map(item => (
-                        <tr key={item.id}>
+                    <tbody>{contacts.length ? contacts.map(contact => (
+                        <tr key={contact.id}>
                             <td className='padding-x-m'>
                                 <input type='checkbox' className='checkbox-default'/>
                             </td>
-                            <td className='flex align-items-center' title={`ID: ${item.id}`}>
-                                {(item.firstName && item.lastName) ?
-                                    <div className='contact-initials margin-right-m color-background-lavender-500 flex justify-content-center align-items-center'>{getInitial(item.firstName)}{getInitial(item.lastName)}</div>
+                            <td>
+                                <span>{contact.id}</span>
+                            </td>
+                            <td className='flex-inline align-items-center'>
+                                {(contact.firstName && contact.lastName) ?
+                                    <div className='contact-initials margin-right-m color-background-lavender-500 flex justify-content-center align-items-center'>
+                                        {getInitial(contact.firstName)}{getInitial(contact.lastName)}
+                                    </div>
                                     : null
                                 }
-                                <span className='color-action'>{item.firstName} {item.lastName}</span>
+                                <span className='color-action'>{contact.firstName} {contact.lastName}</span>
                             </td>
                             <td>
-                                <span></span>
-                            </td>
-                            <td>
-                                <span></span>
+                                <span>{contact.phone}</span>
                             </td>
                             <td>
                                 <span>
-                                    {isLoaded.deals && item.custom ? item.custom.deals.length : 'Loading...'}
+                                    {isLoaded.deals && contact.custom ? contact.custom.deals.length : 'Loading...'}
                                 </span>
                             </td>
-                            <td className={'padding-right-m'}>
-                                <span></span>
+                            <td>
+                                <span>{contact.ip ? contact.ip : null}</span>
+                            </td>
+                            <td className='padding-right-m flex-inline align-items-center'>
+                                {contact.email ?
+                                    <>
+                                        <a href={'mailto:' + contact.email}><button className='button-small button-multi-left'>Email</button></a>
+                                        <button className='button-small button-multi-right'>
+                                            <MenuDownIcon/>
+                                        </button>
+                                    </>
+                                    : null
+                                }
                             </td>
                         </tr>
                     )) : null}
                     </tbody>
                 </table>
 
-                {error.contacts ? <p>Error: {error.contacts.message}</p> : !isLoaded.contacts ? <p>Loading...</p>: null}
+                {error.contacts ? <p className='color-error'>Error: {error.contacts.message}</p> : !isLoaded.contacts ? <p>Loading...</p>: null}
             </>
         );
     }
